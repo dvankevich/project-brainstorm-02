ec437dc88186db1f05f8a24533a44bdd8fedadc9
Valeria_Karanduyk <lera-ka0195@ukr.net>
2024-10-11 16:37:42 +0300

    menu open/close js

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,55 @@
-console.log("header");
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    if (anchors.length === 0) return;
+
+    if (!isMenuOpen) {
+      anchors.forEach(anchor => {
+        anchor.addEventListener('click', toggleMenu);
+      });
+      return;
+    }
+
+    anchors.forEach(anchor => {
+      anchor.removeEventListener('click', toggleMenu);
+    });
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 375px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let href = this.getAttribute('href').substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = document.querySelector('.header-navigation').offsetHeight;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
+  });
+});
