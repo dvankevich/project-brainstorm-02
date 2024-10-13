294c47fa9b7d42c2c35533ac1cfa5be1f2e5a16c
MaksKarpov <ozuzuandvduvatel@gmail.com>
2024-10-14 00:54:37 +0300

    update ready 80

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -66,3 +66,52 @@ async function useReviews() {
 
 // Виклик функції
 useReviews();
+
+
+// Імпортуйте Swiper, якщо ви використовуєте модулі
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css'; // Імпорт стилів Swiper, якщо необхідно
+
+// Налаштування Swiper
+const swiper = new Swiper('.swiper', {
+  // Налаштування для кількості слайдів
+  slidesPerView: 1, // за замовчуванням - 1 слайд
+  spaceBetween: 32, // відстань між слайдами
+
+  // Налаштування для адаптивності
+  breakpoints: {
+    1280: {
+      slidesPerView: 2, // 2 слайди при ширині 1280 пікселів та більше
+    },
+  },
+
+  // Кнопки навігації
+  navigation: {
+    nextEl: '.reviews-arrow-box-right',
+    prevEl: '.reviews-arrow-box-left',
+    disabledClass: 'disabled', // клас для відключених кнопок
+  },
+
+  // Додаткові налаштування (за потреби)
+  loop: false, // Вимкнено зациклення, якщо потрібно
+});
+
+// Функція для оновлення стану кнопок навігації
+function updateNavigation() {
+  const totalSlides = swiper.slides.length;
+  const currentIndex = swiper.activeIndex;
+  const viewportWidth = window.innerWidth;
+  // Увімкнення/вимкнення кнопок навігації
+  swiper.navigation.prevEl.disabled = currentIndex === 0;
+  
+  if (viewportWidth >= 1280) {
+    swiper.navigation.nextEl.disabled = currentIndex === totalSlides - 2;
+    
+  } else {
+    swiper.navigation.nextEl.disabled = currentIndex === totalSlides - 1;
+  }
+  
+}
+
+// Додати слухача подій для оновлення кнопок навігації при зміні активного слайда
+swiper.on('slideChange', updateNavigation);
